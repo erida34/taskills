c38761c97d06e1554e24bd9b7b867ecd19828c24
Artemis-Faul <75532723+Artemis-Faul@users.noreply.github.com>
2021-11-08 07:42:03 +0300

    init project

---
+++ scale-img.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var j = 0;
+    $(".imgonclick").click(function () {
+        var imgGL = $(this).attr("src");
+
+        if (j == 0) {
+            j++;
+            $("#imgonGL").attr("src", imgGL);
+            $("#clickonGL").css({ "z-index": "299", "background-color": "rgba(0, 0, 0, 0.6)" });
+        }
+    });
+    $("#clickonGL").click(function () {
+        if (j == 1) {
+            j--;
+            $("#clickonGL").css({ "z-index": "-1", "background-color": "rgba(0, 0, 0, 0)" });
+            $("#imgonGL").attr("src", "images/index/alfa-img.png");
+        }
+    });
+});
