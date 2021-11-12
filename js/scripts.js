3da44b7a7704eb587c35ca3d707bd3ccdcd802e2
Artemis-Faul <75532723+Artemis-Faul@users.noreply.github.com>
2021-11-12 18:48:29 +0300

    global changes + route.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,21 @@ $(".btn_acc").click(function () {
     $(".modal-window_acc").addClass("display-flex");
 });
 
+$(".btn_change-pass").click(function () {
+    $(".modal-wrap_change").addClass("display-flex");
+    $(".modal-window_change").addClass("display-flex");
+});
+
+$(".btn_exit").click(function () {
+    $(".modal-wrap_exit").addClass("display-flex");
+    $(".modal-window_exit").addClass("display-flex");
+});
 
 // Скрипт закрытия модальных окон
 $(".btn_close").click(function () {
     $(".modal-wrap").removeClass("display-flex");
     $(".modal-window").removeClass("display-flex");
 });
-
 
 // Скрипт перемещения по табам
 $(function () {
@@ -30,7 +38,6 @@ $(function () {
         .click();
 });
 
-
 // Проверка email на валидность
 $("form input[type=email]").on("input", function () {
     var pattern = /^[a-z0-9_.-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
@@ -46,7 +53,6 @@ $("form input[type=email]").on("input", function () {
         }
     }
 });
-
 
 // Выпадающее меню
 $(".drop-menu").hide();
@@ -66,4 +72,53 @@ $(".drop-menu__link").click(function () {
     }
 });
 
+// Превью фотографии при добавлении
 
+$(".uploaded-file").on("change", function () {
+    getFileParam();
+    $(".footer").css({ "margin-top": "5vh" });
+});
+
+function getFileParam() {
+    try {
+        var file = document.getElementsByClassName("uploaded-file").item(0).files[0];
+        if (file) {
+            if (/\.(jpe?g|bmp|gif|png)$/i.test(file.name)) {
+                var elPreview = document.getElementById("preview");
+                elPreview.innerHTML = "";
+                var newImg = document.createElement("img");
+                newImg.className = "preview-img";
+
+                if (typeof file.getAsDataURL == "function") {
+                    if (file.getAsDataURL().substr(0, 11) == "data:image/") {
+                        newImg.setAttribute("src", file.getAsDataURL());
+                        elPreview.appendChild(newImg);
+                    }
+                } else {
+                    var reader = new FileReader();
+                    reader.onloadend = function (evt) {
+                        if (evt.target.readyState == FileReader.DONE) {
+                            newImg.setAttribute("src", evt.target.result);
+                            elPreview.appendChild(newImg);
+                        }
+                    };
+
+                    var blob;
+                    if (file.slice) {
+                        blob = file.slice(0, file.size);
+                    } else if (file.webkitSlice) {
+                        blob = file.webkitSlice(0, file.size);
+                    } else if (file.mozSlice) {
+                        blob = file.mozSlice(0, file.size);
+                    }
+                    reader.readAsDataURL(blob);
+                }
+            }
+        }
+    } catch (e) {
+        alert("file3");
+        var file = document.getElementsByClassName("uploaded-file").item(0).value;
+        file = file.replace(/\\/g, "/").split("/").pop();
+        document.getElementById("file-name").innerHTML = file;
+    }
+}
